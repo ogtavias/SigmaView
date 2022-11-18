e283c484c54bf1e97b10ffeced7751d7a3c332f0
Vasyl <ogtavias@gmail.com>
2022-11-18 21:10:43 +0200

    Upload sigma

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.top-block').find('video').trigger('play');
+	var initVideo = function () {
+		$('.top-block').find('video').trigger('play');
+	};
+});
+
+var initVideo = function () {
+	$('.top-block').find('video').trigger('play');
+};
